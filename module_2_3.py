beb1145ded3a2b64828a72a0200dd925835100e7
KaterinaDvoriagina <ekaterima786@gmail.com>
2024-12-16 01:29:52 +0300

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,13 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6]
+while my_list:
+    if my_list[0]:
+        print(my_list[0])
+    if my_list[1]:
+        print(my_list[1])
+    if my_list[2]:
+        print(my_list[2])
+    if my_list[3]:
+        print(my_list[3])
+    if my_list[5]:
+        print(my_list[5])
+    break
